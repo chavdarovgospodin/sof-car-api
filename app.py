4aebdb60c452631a82f723fa432b1265dba69585
chavdarovgospodin <g.chavdarov92@gmail.com>
2025-08-29 13:00:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'Work in progress!!!!!!!\n'
+    message = 'Work in progress!\n'
     version = 'Python v' + sys.version.split()[0] + '\n'
     response = '\n'.join([message, version])
     return [response.encode()]
